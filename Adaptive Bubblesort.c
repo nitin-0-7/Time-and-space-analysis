eaf805d00403b558fd6edd3bf1a8b773de707ca1
Nitin kumar <nitinkrc2432@gmail.com>
2025-08-02 14:39:17 +0530

    Adaptive Bubblesort

---
+++ Adaptive Bubblesort.c
@@ -0,0 +1,45 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+void PrintArray(int arr[],int n){
+    for(int i=0;i<n;i++){
+        printf("%d ",arr[i]);
+    }
+    printf("\n");
+}
+void Bubblesort(int arr[],int n){
+    int isSorted=0;
+    for(int i=0;i<n;i++){
+        printf("Working on pass number %d\n",i+1);
+        isSorted=1;
+        for(int j=0;j<n;j++){
+            if(arr[j]>arr[j+1]){
+                int temp=arr[j+1];
+                arr[j+1]=arr[j];
+                arr[j]=temp;
+                isSorted=0;
+            }
+        }
+        if(isSorted){
+            return;
+        }
+    }
+}
+
+int main()
+{
+    int arr[9]={15,35,67,78,34,91,43,87,55};
+    int n=9;
+    PrintArray(arr,n);
+    Bubblesort(arr,n);
+    PrintArray(arr,n);
+    
+
+    return 0;
+}
